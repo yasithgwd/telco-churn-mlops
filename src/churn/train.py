4d4ad991e0401b4c253be666caec93f256807785
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2026-02-04 15:03:13 +0530

    chore: model training on log reg

---
+++ src/churn/train.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LogisticRegression
+
+def train_logreg_model(X_train, y_train, random_state: int = 42) -> LogisticRegression:
+    """
+    Train a logistic regression model
+    """
+    model = LogisticRegression(
+        random_state=random_state, 
+        max_iter=2000,
+        class_weight='balanced',
+        n_jobs=None
+        )
+    
+    model.fit(X_train, y_train)
+    return model
