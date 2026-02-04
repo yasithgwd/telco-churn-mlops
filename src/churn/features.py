75f9c1369e11c2ff17778360d87e2133deddcbc5
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2026-02-04 14:57:20 +0530

    chore: preprocessing stuff

--- src/churn/features.py
+++ src/churn/features.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from typing import Tuple 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
 
 def split_features_traget(
         df: pd.DataFrame, target_col: str
@@ -14,4 +18,34 @@ def split_features_traget(
 
     return X, y
 
+def preprocessing_pipeline(X: pd.DataFrame) -> ColumnTransformer:
+    """
+    Build a preprocessing transformer
+    - Numeric: impute + scale 
+    - categorical : impute + one-hot encode
+    """
+    numeric_cols = X.select_dtypes(include=["number"]).columns.tolist()
+    categorical_cols = X.select_dtypes(include=["object", "category", "bool"]).columns.tolist()
 
+    numeric_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="median")),
+            ("scaler", StandardScaler())
+        ]
+    )
+
+    categorical_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("onehot", OneHotEncoder(handle_unknown="ignore"))
+        ]
+    )
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, numeric_cols),
+            ("cat", categorical_transformer, categorical_cols)
+        ]
+    )
+
+    return preprocessor
